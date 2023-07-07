7f39bb8f44a68cc9097a3ac6a4a4e20378f3043a
Khristina <k.svirska@4irelabs.com>
2023-07-07 14:09:22 +0300

    'testFirst

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Test from './components/Test';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>HELLO</h3>
+  <Test/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Test/index.jsx
@@ -0,0 +1,13 @@
+const Test = () => {
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <h2>HELLO</h2>
+      <h3>HELLO</h3>
+      <h3>HELLO</h3>
+      <h4>HELLO</h4>
+      <h5>HELLO</h5>
+    </div>
+  );
+};
+export default Test;
